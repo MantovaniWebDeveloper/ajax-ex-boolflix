871458b88cf9a0eab154fc434616bf33ac8cdf5c
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2018-12-17 15:17:51 +0100

    funzione con chiamata ajax che prende come argomento il valoreRicerca

--- js/main.js
+++ js/main.js
@@ -5,23 +5,28 @@ $(document).ready(function(){
     //recupero il valore dalla barra di ricerca
     var valoreRicerca = $("#barraRicerca").val();
     console.log(valoreRicerca);
+    cercaFilm(valoreRicerca);
   });
+  //funzione con chimata ajx che prende come argomento
+  //il valore della barra di ricerca
+  function cercaFilm(valoreRicerca){
+    $.ajax({
+      url: 'https://api.themoviedb.org/3/search/movie',
+      method: 'GET',
+      data: {
+        api_key: '625a162fa4a03ee07f61388c8fe99cd2',
+        language: 'it',
+        query: valoreRicerca
+      },
+      success: function(data, stato) {
+        console.log(data)
 
-  $.ajax({
-    url: 'https://api.themoviedb.org/3/search/movie',
-    method: 'GET',
-    data: {
-      api_key: '625a162fa4a03ee07f61388c8fe99cd2',
-      language: 'it',
-      query: valoreRicerca
-    },
-    success: function(data, stato) {
-      console.log(data)
+      },
+      error: function(richiesta, stato, errori) {
+        console.log("c'è stato un errore " + errori);
+      }
+    });
+  }
 
-    },
-    error: function(richiesta, stato, errori) {
-      console.log("c'è stato un errore " + errori);
-    }
-  });
 
 });
